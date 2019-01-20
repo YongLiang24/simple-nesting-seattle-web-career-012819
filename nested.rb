e99d92a6e5aa7c05dc2dd2d58bdc55aa410ce5a3
yong liang <yongliang24@gmail.com>
2019-01-20 22:49:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,8 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		x= programmer_hash[:dennis_ritchie][:language][0]
-		return x
+		return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
